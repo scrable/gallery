eeae66f78376d1ecb107fe51907e3de6792e0469
Corey Russ <cruss.seq11@gmail.com>
2021-05-10 00:31:18 -0700

    add thumbnail generator

--- routes/home.js
+++ routes/home.js
@@ -10,31 +10,53 @@ exports.list = function(req, res){
      });
      const isImage = require('is-image');
 
+     var thumb = require('node-thumbnail').thumb;
+     thumb({
+          source: 'public/images',
+          destination: 'public/thumbnails',
+          concurrency: 8,
+          width: 450,
+          ignore: true,
+          suffix: ''
+     }).then (function() {
+          console.log('All done!');
+     }).catch(function (e){
+          console.log('Error', e.toString());
+     });
+
+     const thumbs = fs.readdirSync("public/thumbnails");
+
      for(i = 0; i < files.length; i++){
           if(!isImage(files[i])){
                files.splice(i, 1)
           }
      }
+     for(i = 0; i < thumbs.length; i++){
+          if(!isImage(thumbs[i])){
+               thumbs.splice(i, 1)
+               console.log("deleting item from thumbs " + i)
+          }
+     }
 
      var sizeOf = require('image-size');
 
-          function getsizes(path){
-               if(isImage(path)) {
-                    var y = sizeOf(path)
+     function getsizes(path){
+          if(isImage(path)) {
+               var y = sizeOf(path)
 
-                    if (parseInt(y.orientation) === 6) {
-                         widths.push(JSON.parse(JSON.stringify(y.height)))
-                         heights.push(JSON.parse(JSON.stringify(y.width)))
-                    } else {
-                         heights.push(JSON.parse(JSON.stringify(y.height)))
-                         widths.push(JSON.parse(JSON.stringify(y.width)))
-                    }
+               if (parseInt(y.orientation) === 6) {
+                    widths.push(JSON.parse(JSON.stringify(y.height)))
+                    heights.push(JSON.parse(JSON.stringify(y.width)))
+               } else {
+                    heights.push(JSON.parse(JSON.stringify(y.height)))
+                    widths.push(JSON.parse(JSON.stringify(y.width)))
                }
           }
+     }
 
-          for(var i = 0; i < files.length; i++) {
-               getsizes('public/images/' + files[i])
-          }
+     for(var i = 0; i < files.length; i++) {
+          getsizes('public/images/' + files[i])
+     }
 
-     res.render('homePage', {data: files, heights: heights, widths: widths})
+     res.render('homePage', {data: files, heights: heights, widths: widths, thumbs: thumbs})
 };
